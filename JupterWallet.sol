5e35eb0e786f8ebe0b065c9e9c5cb8eaf98b5cac
DevGodPlanet <82236390+DevGodPlanet@users.noreply.github.com>
2021-04-11 02:59:34 -0300

    Update JupterWallet.sol

--- JupterWallet.sol
+++ JupterWallet.sol
@@ -10,7 +10,6 @@
    3% fee auto add to the liquidity pool to locked forever when selling
    2% fee auto distribute to all holders
    I created a black hole so #Bee token will deflate itself in supply with every transaction
-   50% Supply is burned at start.
    
  */
 
